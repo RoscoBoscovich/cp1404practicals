52fbeffda3e4d17f2c4fff8e907b8e2fa8cbb762
RoscoBoscovich <41560046+RoscoBoscovich@users.noreply.github.com>
2018-08-08 09:36:08 +1000

    temperature program updated to use functions

--- Prac3/temperatures.py
+++ Prac3/temperatures.py
@@ -3,25 +3,42 @@ CP1404/CP5632 - Practical
 Pseudocode for temperature conversion
 """
 
-MENU = """C - Convert Celsius to Fahrenheit
-F - Convert Fahrenheit to Celsius
-Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-        # TODO: Write this section to convert F to C and display the result
-        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-        # Remove the "pass" statement when you are done. It's a placeholder.
-        fahrenheit = float(input("Fahrenheit: "))
-        celcius = (fahrenheit - 32) *  (5 / 9)
-        print("Result: {:.2f} C".format(celcius))
-    else:
-        print("Invalid option")
+
+def temp_converter():
+    MENU = """    C - Convert Celsius to Fahrenheit
+    F - Convert Fahrenheit to Celsius
+    Q - Quit"""
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            celcius = float(input("Celsius: "))
+            fahrenheit = celcius_to_far(celcius)
+            print("Result: {:.2f} F".format(fahrenheit))
+
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celcius = far_to_celcius(fahrenheit)
+            print("Result: {:.2f} C".format(celcius))
+
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+def celcius_to_far(celcius):
+    fahrenheit = celcius * 9.0 / 5 + 32
+    return fahrenheit
+
+def far_to_celcius(fahrenheit):
+    celcius = (fahrenheit - 32) * (5 / 9)
+    return celcius
+
+
+
+temp_converter()
+
+
+
+
