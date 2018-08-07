f5e411cbf4da27f7525df1c26c97ed0a5d451f9c
RoscoBoscovich <41560046+RoscoBoscovich@users.noreply.github.com>
2018-08-08 09:41:50 +1000

    score program updated to use functions

--- Prac3/broken_score.py
+++ Prac3/broken_score.py
@@ -3,14 +3,25 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
+def score_output():
+    score = float(input("Enter score: "))
+    judgement = score_checker(score)
+    print(judgement)
 
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 50 and score < 90:
-        print("Passable")
-elif score >= 90:
-        print("Excellent")
-elif score < 50:
-        print("Bad")
+def score_checker(score):
+    if score < 0 or score > 100:
+        judgement = "Invalid score"
+    elif score >= 50 and score < 90:
+        judgement = "Passable"
+    elif score >= 90:
+        judgement = "Excellent"
+    elif score < 50:
+        judgement= "Bad"
+    return judgement
+
+
+score_output()
+
+
+
 
