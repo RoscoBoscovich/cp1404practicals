f716033c70523e7d978e7f05a19bfdfbb3aaff26
RoscoBoscovich <41560046+RoscoBoscovich@users.noreply.github.com>
2018-08-08 10:32:33 +1000

    word generator updated to check validity of input

---
+++ Prac3/word_generator.py
@@ -0,0 +1,45 @@
+"""
+CP1404/CP5632 - Practical
+Random word generator - based on format of words
+Another way to get just consonants would be to use string.ascii_lowercase
+(all letters) and remove the vowels.
+"""
+
+def word_gen_main():
+    import random
+
+    VOWELS = "aeiou"
+    CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+
+    word_format = input("""Please enter sting for word generator""")
+    validity = is_valid_format(word_format)
+
+    if validity == False:
+        print("Invalid input!")
+
+    while validity == True:
+
+        word = ""
+        for kind in word_format:
+              if kind == "c":
+                 word += random.choice(CONSONANTS)
+              else:
+                 word += random.choice(VOWELS)
+        validity = False
+        print(word)
+
+
+
+def is_valid_format(word_format):
+    for kind in word_format:
+        if (kind == "c") or (kind == "v"):
+            valid_flag = True
+        else:
+            return False
+    return valid_flag
+
+
+
+
+
+word_gen_main()
